ae80826f9ab062352f1ab39a1c0a666b4cdadcba
pintudas11 <pintu.das11@gmail.com>
2015-10-22 14:06:58 -0600

    Update Config.java

--- src/main/java/com/gopivotal/cf/workshop/Config.java
+++ src/main/java/com/gopivotal/cf/workshop/Config.java
@@ -19,6 +19,7 @@ public class Config {
     public Cloud cloudFactory() {
 
     	try {
+    	    String abc="assasas";
     		return new CloudFactory().getCloud();
     	} catch ( CloudException ce ) {
     		//	Not running in cloud environment, return null
